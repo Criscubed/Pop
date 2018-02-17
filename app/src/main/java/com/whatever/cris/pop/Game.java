582a56e38e9a4a5385a68a0bd47042fdaf35ce7e
Criscubed <criscubed@yahoo.com>
2018-02-17 02:58:58 +0100

    You can adjust the FPS now

--- app/src/main/java/com/whatever/cris/pop/Game.java
+++ app/src/main/java/com/whatever/cris/pop/Game.java
@@ -27,6 +27,10 @@ public class Game extends SurfaceView implements Runnable {
     public static final float NANOS_TO_MILLIS  = 1.0f/MILLIS_TO_NANOS;
     public static final float NANOS_TO_SECONDS = 1.0f/SECONDS_TO_NANOS;
     public static final long SAMPLE_INTERVAL = 1 * SECONDS_TO_NANOS;
+    public static final long TARGET_FRAMERATE = 30;
+    public static final long MS_PER_FRAME = 1000/TARGET_FRAMERATE;
+    public static final long  NANOS_PER_FRAME = MS_PER_FRAME * MILLIS_TO_NANOS;
+
     private long mLastSampleTime = 0;
     private long mFrameCount = 0;
     private float mAvgFramerate = 0;
@@ -107,10 +111,18 @@ public class Game extends SurfaceView implements Runnable {
     @Override
     public void run() {
         while(mIsRunning){
+            long start = System.nanoTime();
             onEnterFrame();
             input();
             update();
             render();
+            float millisRemaining = ((start + NANOS_PER_FRAME) - System.nanoTime()) * NANOS_TO_MILLIS;
+            if(millisRemaining > 1){
+                try {
+                    Thread.sleep((long)millisRemaining);
+                } catch (InterruptedException e) {
+                }
+            }
         }
     }
 
