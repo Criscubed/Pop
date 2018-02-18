906ad0508d1edbc7e1bd64578121bcd5ae887178
Criscubed <criscubed@yahoo.com>
2018-02-18 18:25:50 +0100

    Optimized the way bitmaps get displayed mostly
    Deleted a bunch of unecessary code that i didnt even write. Also you can
    flip bitmaps now.

--- app/src/main/java/com/whatever/cris/pop/Enemy.java
+++ app/src/main/java/com/whatever/cris/pop/Enemy.java
@@ -16,6 +16,7 @@ import android.util.Log;
 public class Enemy extends Entity {
     public static final String TAG = "Enemy";
     public static final float ENEMY_MAX_SPEED = 3;
+    public static final int ENEMY_HEIGHT = 50;
     Bitmap mBitmap;
 
     public Enemy(Context context){
@@ -36,61 +37,33 @@ public class Enemy extends Entity {
                 Log.w(TAG, "You messed up your EnemyID" + select);
                 break;
         }
-        mBitmap = decodeSampledBitmapFromResource(context.getResources(), resourceId,50,50);
+        Bitmap temp = BitmapFactory.decodeResource(context.getResources(), resourceId);
+        mBitmap = Utils.scaleToTargetHeight(temp ,  ENEMY_HEIGHT);
+        mBitmap = Utils.flipBitmap(mBitmap, false);
+        if(temp != mBitmap){
+            temp.recycle();
+        }
+        respawn();
+
+    }
+
+    public void respawn(){
         mHeight = mBitmap.getHeight();
         mWidth = mBitmap.getWidth();
         mY = mDice.nextInt(Game.STAGE_HEIGHT - (int) mHeight);
         mX = Game.STAGE_WIDTH + mDice.nextInt((int)mWidth);
         mVelocityX = -1 + -mDice.nextInt((int)ENEMY_MAX_SPEED);
-
-
-
     }
-
+    @Override
+    public void worldWrap(final float width, final float height){
+        if(mX < -mWidth){
+            respawn();
+        }
+    }
 
     @Override
     public void render(final Canvas canvas,final Paint paint) {
         canvas.drawBitmap(mBitmap, mX, mY, paint);
     }
 
-    //The following two methods were borrowed from
-    //https://developer.android.com/topic/performance/graphics/load-bitmap.html
-
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
-    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                         int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
 }
--- app/src/main/java/com/whatever/cris/pop/Utils.java
+++ app/src/main/java/com/whatever/cris/pop/Utils.java
@@ -1,5 +1,9 @@
 package com.whatever.cris.pop;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+
 public class Utils {
 
     public static float wrap(float val, final float min, final float max){
@@ -9,6 +13,35 @@ public class Utils {
             val = min;
         }
         return val;
+    }
+
+    public static float clamp(float val, final float min, final float max){
+        if(val < min){
+            val = min;
+        } else if(val > max){
+            val = max;
+        }
+        return val;
+    }
+
+    public static Bitmap flipBitmap(Bitmap source, boolean horizontally){
+        Matrix m = new Matrix();
+        int cx = source.getWidth()/2;
+        int cy = source.getWidth()/2;
+        if(horizontally){
+            m.postScale(1, -1, cx, cy);
+        } else {
+            m.postScale(-1, 1, cx, cy);
+        }
+        return Bitmap.createBitmap(source, 0, 0,
+                source.getWidth(), source.getHeight(),
+                m, true);
+    }
+    public static Bitmap scaleToTargetHeight(Bitmap source, int height){
+        float ratio = height / (float) source.getHeight();
+        int newHeight = (int) (source.getHeight() * ratio);
+        int newWidth = (int) (source.getWidth() * ratio);
+        return Bitmap.createScaledBitmap(source, newWidth, newHeight, true);
     }
 
 }
